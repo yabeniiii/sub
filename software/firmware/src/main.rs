56fb01446f0537d447f6539101af91e2165fa3fe
yabeni <yabeni@yabeni.eu>
2026-07-24 12:55:05 +0200

    add firmware crate

---
+++ software/firmware/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let _p = embassy_stm32::init(Default::default());
+    loop {
+        defmt::info!("Blink");
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
