fed36e3d53c215b3cb49747854e8a17cb4e0e803
yabeni <yabeni@yabeni.eu>
2026-07-26 15:21:44 +0200

    AT utility refactor

--- software/firmware/src/main.rs
+++ software/firmware/src/main.rs
@@ -6,15 +6,32 @@ use core::str::from_utf8;
 use crate::managers::comms::ATCommand;
 use crate::managers::comms::ATResponse;
 use crate::managers::comms::CommsManager;
-use crate::managers::comms::CommsManagerPeripherals;
 use defmt::error;
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_stm32::Peri;
+use embassy_stm32::bind_interrupts;
+use embassy_stm32::dma;
+use embassy_stm32::gpio;
+use embassy_stm32::mode::Async;
+use embassy_stm32::peripherals;
+use embassy_stm32::usart;
+use embassy_stm32::usart::Uart;
+use embassy_stm32::usart::UartRx;
+use embassy_stm32::usart::UartTx;
 use embassy_time::Timer;
 
 use {defmt_rtt as _, panic_probe as _};
 
-const SENSOR_NUMBER: usize = 3;
+// const SENSOR_NUMBER: usize = 3;
+
+bind_interrupts!(struct Irqs {
+    USART1 => usart::InterruptHandler<peripherals::USART1>;
+    DMA2_STREAM7 => dma::InterruptHandler<peripherals::DMA2_CH7>;
+    DMA2_STREAM5 => dma::InterruptHandler<peripherals::DMA2_CH5>;
+});
+
+static mut DMA_BUF: [u8; 4096] = [0u8; 4096];
 
 // mod actuators;
 mod managers;
@@ -37,19 +54,54 @@ mod managers;
 //     loop {}
 // }
 
+struct CommsManagerPeripherals {
+    reset_pin: Peri<'static, peripherals::PA8>,
+    usart_channel: Peri<'static, peripherals::USART1>,
+    rx_pin: Peri<'static, peripherals::PA10>,
+    tx_pin: Peri<'static, peripherals::PA9>,
+    tx_dma: Peri<'static, peripherals::DMA2_CH7>,
+    rx_dma: Peri<'static, peripherals::DMA2_CH5>,
+}
+
+async fn init_modem_uart(
+    p: CommsManagerPeripherals,
+) -> (UartTx<'static, Async>, UartRx<'static, Async>) {
+    let mut reset = gpio::Output::new(p.reset_pin, gpio::Level::High, gpio::Speed::Low);
+
+    reset.toggle();
+    Timer::after_millis(20).await;
+    reset.toggle();
+
+    Timer::after_secs(3).await;
+
+    Uart::new(
+        p.usart_channel,
+        p.rx_pin,
+        p.tx_pin,
+        p.tx_dma,
+        p.rx_dma,
+        Irqs,
+        usart::Config::default(),
+    )
+    .unwrap()
+    .split()
+}
+
 #[embassy_executor::task]
 async fn comms_manager_thread(p: CommsManagerPeripherals) {
-    let mut manager = CommsManager::new(p).await;
+    let (tx, rx) = init_modem_uart(p).await;
+    let mut manager = CommsManager::new(tx, rx);
 
     let mut response = [0u8; 128];
     match manager.send_at(ATCommand::ATE0, &mut response).await {
+        ATResponse::BufferFull => error!("Buffer full"),
         ATResponse::SendError(e) => error!("Send Error for ATE0: {}", e),
         ATResponse::ReceiveError(e) => error!("Receive Error for AT: {}", e),
-        ATResponse::Ok { bytes } => info!(
+        ATResponse::Ok(bytes) => info!(
             "response to ATE0: {}",
             from_utf8(&response[..bytes]).unwrap().trim_ascii()
         ),
-        ATResponse::Error { bytes } => error!(
+        ATResponse::Error(bytes) => error!(
             "error response to ATE0: {}",
             from_utf8(&response[..bytes]).unwrap().trim_ascii()
         ),
@@ -59,20 +111,21 @@ async fn comms_manager_thread(p: CommsManagerPeripherals) {
         }
     };
 
+    response.fill(0);
     match manager.send_at(ATCommand::AT, &mut response).await {
+        ATResponse::BufferFull => error!("Buffer full"),
         ATResponse::SendError(e) => error!("Send Error for AT: {}", e),
         ATResponse::ReceiveError(e) => error!("Receive Error for AT: {}", e),
-        ATResponse::Ok { bytes } => info!(
+        ATResponse::Ok(bytes) => info!(
             "response to AT: {}",
             from_utf8(&response[..bytes]).unwrap().trim_ascii()
         ),
-        ATResponse::Error { bytes } => error!(
+        ATResponse::Error(bytes) => error!(
             "error response to AT: {}",
             from_utf8(&response[..bytes]).unwrap().trim_ascii()
         ),
         ATResponse::Timeout => {
             error!("timeout on AT");
-            return;
         }
     };
 }
@@ -85,17 +138,16 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Submarine booting...");
 
-    spawner.spawn(
-        comms_manager_thread(CommsManagerPeripherals {
-            reset_pin: p.PA8,
-            usart_channel: p.USART1,
-            rx_pin: p.PA10,
-            tx_pin: p.PA9,
-            tx_dma: p.DMA2_CH7,
-            rx_dma: p.DMA2_CH5,
-        })
-        .unwrap(),
-    );
+    let comms_peripherals = CommsManagerPeripherals {
+        reset_pin: p.PA8,
+        usart_channel: p.USART1,
+        rx_pin: p.PA10,
+        tx_pin: p.PA9,
+        tx_dma: p.DMA2_CH7,
+        rx_dma: p.DMA2_CH5,
+    };
+
+    spawner.spawn(comms_manager_thread(comms_peripherals).unwrap());
     // spawner.spawn(sensor_manager_thread().unwrap());
     // spawner.spawn(actuator_manager_thread().unwrap());
 
--- software/firmware/src/managers/comms.rs
+++ software/firmware/src/managers/comms.rs
@@ -1,29 +1,15 @@
-use core::str::FromStr;
+use crate::DMA_BUF;
 
 use defmt::error;
 use defmt::info;
 use defmt::warn;
-use embassy_stm32::Peri;
-use embassy_stm32::bind_interrupts;
-use embassy_stm32::dma;
-use embassy_stm32::gpio;
 use embassy_stm32::mode::Async;
-use embassy_stm32::peripherals;
 use embassy_stm32::usart;
 use embassy_stm32::usart::RingBufferedUartRx;
-use embassy_stm32::usart::Uart;
+use embassy_stm32::usart::UartRx;
 use embassy_stm32::usart::UartTx;
 use embassy_time::Duration;
-use embassy_time::Timer;
 use embassy_time::with_timeout;
-
-bind_interrupts!(struct Irqs {
-    USART1 => usart::InterruptHandler<peripherals::USART1>;
-    DMA2_STREAM7 => dma::InterruptHandler<peripherals::DMA2_CH7>;
-    DMA2_STREAM5 => dma::InterruptHandler<peripherals::DMA2_CH5>;
-});
-
-static mut DMA_BUF: [u8; 4096] = [0u8; 4096];
 
 // pub enum Message<'a> {
 //     Error(heapless::String<64>),
@@ -36,57 +22,54 @@ pub enum ATCommand {
     ATE0,
 }
 
+impl ATCommand {
+    fn bytes(&self) -> &'static [u8] {
+        match self {
+            ATCommand::AT => b"AT\r\n",
+            ATCommand::ATE0 => b"ATE0\r\n",
+        }
+    }
+
+    fn timeout(&self) -> Duration {
+        match self {
+            ATCommand::AT => Duration::from_millis(100),
+            ATCommand::ATE0 => Duration::from_millis(100),
+        }
+    }
+}
+
 pub enum ATResponse {
+    BufferFull,
     SendError(usart::Error),
     ReceiveError(usart::Error),
-    Ok { bytes: usize },
-    Error { bytes: usize },
+    Ok(usize),
+    Error(usize),
     Timeout,
 }
 
-pub struct CommsManagerPeripherals {
-    pub reset_pin: Peri<'static, peripherals::PA8>,
-    pub usart_channel: Peri<'static, peripherals::USART1>,
-    pub rx_pin: Peri<'static, peripherals::PA10>,
-    pub tx_pin: Peri<'static, peripherals::PA9>,
-    pub tx_dma: Peri<'static, peripherals::DMA2_CH7>,
-    pub rx_dma: Peri<'static, peripherals::DMA2_CH5>,
+impl ATResponse {
+    fn is_complete(buffer: &[u8]) -> Option<Self> {
+        if buffer.windows(6).any(|w| w == b"\r\nOK\r\n") {
+            Some(Self::Ok(buffer.len()))
+        } else if buffer.windows(9).any(|w| w == b"\r\nERROR\r\n") {
+            Some(Self::Error(buffer.len()))
+        } else {
+            None
+        }
+    }
 }
 
 pub struct CommsManager<'a> {
-    uart_reset: gpio::Output<'a>,
     uart_transmitter: UartTx<'a, Async>,
     uart_receiver: RingBufferedUartRx<'a>,
 }
 
 impl<'a> CommsManager<'a> {
-    pub async fn new(p: CommsManagerPeripherals) -> Self {
-        let config = usart::Config::default(); // 115200 baud, 8N1  
-
-        let mut reset = gpio::Output::new(p.reset_pin, gpio::Level::High, gpio::Speed::Low);
-        reset.toggle();
-        Timer::after_millis(20).await;
-        reset.toggle();
-
-        Timer::after_secs(3).await;
-
-        let usart = Uart::new(
-            p.usart_channel,
-            p.rx_pin, // RX
-            p.tx_pin, // TX
-            p.tx_dma, // TX DMA
-            p.rx_dma, // RX DMA
-            Irqs,
-            config,
-        )
-        .unwrap();
-
-        let (tx, rx) = usart.split();
+    pub fn new(tx: UartTx<'a, Async>, rx: UartRx<'a, Async>) -> Self {
         let mut rx = rx.into_ring_buffered(unsafe { &mut *core::ptr::addr_of_mut!(DMA_BUF) });
         rx.start_uart();
 
         CommsManager {
-            uart_reset: reset,
             uart_transmitter: tx,
             uart_receiver: rx,
         }
@@ -106,24 +89,21 @@ impl<'a> CommsManager<'a> {
     //     }
     // }
 
+    pub async fn drain_buffer(&mut self) {}
+
     pub async fn send_at(&mut self, command: ATCommand, response_buffer: &mut [u8]) -> ATResponse {
-        let timeout = match command {
-            ATCommand::AT => {
-                if let Err(e) = self.uart_transmitter.write(b"AT\r\n").await {
-                    return ATResponse::SendError(e);
-                }
-                Duration::from_millis(100)
-            }
-            ATCommand::ATE0 => {
-                if let Err(e) = self.uart_transmitter.write(b"ATE0\r\n").await {
-                    return ATResponse::SendError(e);
-                }
-                Duration::from_millis(100)
-            }
-        };
+        let timeout = command.timeout();
+
+        if let Err(e) = self.uart_transmitter.write(command.bytes()).await {
+            return ATResponse::SendError(e);
+        }
 
         let mut index = 0;
         loop {
+            if index >= response_buffer.len() {
+                return ATResponse::BufferFull;
+            }
+
             let n = match with_timeout(
                 timeout,
                 self.uart_receiver.read(&mut response_buffer[index..]),
@@ -139,16 +119,8 @@ impl<'a> CommsManager<'a> {
             };
             index += n;
 
-            if response_buffer[..index]
-                .windows(6)
-                .any(|w| w == b"\r\nOK\r\n")
-            {
-                return ATResponse::Ok { bytes: index };
-            } else if response_buffer[..index]
-                .windows(9)
-                .any(|w| w == b"\r\nERROR\r\n")
-            {
-                return ATResponse::Error { bytes: index };
+            if let Some(response) = ATResponse::is_complete(&response_buffer[..index]) {
+                return response;
             }
         }
     }
